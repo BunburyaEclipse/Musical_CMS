ada3fa19e14615959076ef59c8bef3682b348371
PseudoLoki <sirbastidas09@gmail.com>
2024-03-03 13:32:20 -0500

    optimizado js

--- storage/static/js/main.js
+++ storage/static/js/main.js
@@ -1,7 +1,13 @@
 const menuBtn = document.getElementById('menuBtn');
 const menu = document.getElementById("menu");
+const modal_btn = document.getElementById("modal_info_btn");
 
 menuBtn.addEventListener("click", ()=>{
 	menu.classList.toggle("showMenu")
 	menu.classList.toggle("translate-x-[-1200px]")
-});
+}); 
+
+modal_btn.addEventListener("click", ()=>{
+	menu.classList.remove("showMenu");
+	menu.classList.add("translate-x-[-1200px]");
+})
